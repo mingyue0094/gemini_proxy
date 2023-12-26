cc0526ea7ce9f50f1618a09135527d4b2f1f078d
mingyue0094 <shmily_006@qq.com>
2023-12-26 14:38:15 +0800

    Update main.go

--- main.go
+++ main.go
@@ -11,16 +11,20 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
 	"github.com/sirupsen/logrus"
+
 	"sync"
 )
 
 var (
 	log     *logrus.Logger
 	logFile *os.File
+
 	client     *http.Client
 	clientInit sync.Once
-	post_url   string
+	post_url   string //stream请求网址
+	post_url_  string //普通请求网址
 )
 
 // Message 结构体用于解析messages中的消息
@@ -35,14 +39,140 @@ type Data_body struct {
 	Model    string         `json:"model"`
 }
 
+// stream 返回相关
+type Delta struct {
+	Content string `json:"content"`
+}
+
+type Choice struct {
+	Delta Delta `json:"delta"`
+}
+
+type MyStruct struct {
+	Choices []Choice `json:"choices"`
+}
+
 func main() {
 	log_init()
 	defer logFile.Close()
 
 	http.HandleFunc("/v1/chat/completions", HandleGenerateRequest)
-	port := ":8080"
+	http.HandleFunc("/fyapp", HandlehcfyappRequest)
+	port := ":8883"
 	log.Printf("Server is listening on port %s...\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+// 提供hcfy.app 翻译服务。 只能用于几句话的，不能用来自动整页翻译。
+func HandlehcfyappRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var jsonBody map[string]interface{}
+
+	// 解析JSON数据
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&jsonBody); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	// 输出JSON数据到控制台
+	log.Debugln("Received JSON:", jsonBody)
+
+	// 要翻译的文本
+	text := jsonBody["text"].(string)
+
+	// abc翻译结果
+	abc := gemini_text_rqeusts(text)
+
+	// 返回JSON响应
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	responseJSON := map[string]interface{}{
+		"text":   "你好，划词翻译",
+		"from":   "英语",
+		"to":     "中文",
+		"result": []string{abc},
+	}
+	json.NewEncoder(w).Encode(responseJSON)
+}
+
+// gemini 普通请求
+func gemini_text_rqeusts(prompt string) string {
+
+	// 构造 payload
+	payload := fmt.Sprintf(`{"contents":[{"parts":[{"text": "请把以下内容翻译为中文\n\n'''\n%s\n\n'''"}]}]}`, prompt)
+
+	log.Debugln("提交内容：", prompt)
+	// Ensure client is initialized
+	clientInit.Do(InitializeGenerativeClient)
+
+	req, err := http.NewRequest("POST", post_url_, bytes.NewBuffer([]byte(payload)))
+	if err != nil {
+		log.Debugln("Error creating request:", err)
+
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("请求错误:", err)
+		return ""
+	}
+	defer resp.Body.Close()
+
+	log.Debugln("状态码:", resp.Status)
+
+	// 处理响应
+	if resp.StatusCode == http.StatusOK {
+
+		// 使用 Scanner 逐行读取响应内容
+		log.Debugln("fyapp Request successful. Response:")
+		scanner := bufio.NewScanner(resp.Body)
+		for scanner.Scan() {
+			line := scanner.Text()
+			// 如果行中包含 "text"，则打印该行
+			if strings.Contains(line, "text") {
+
+				line = strings.Replace(line, `\"`, `"`, -1)
+				log.Debugln(line)
+				a := len(line)
+				log.Debugln(a)
+
+				b := strings.Index(line, "text")
+
+				log.Debugln(b)
+
+				// 删除开头和结尾多余的
+				c := line[b+11 : a-4]
+
+				// 去除多余符号
+				c = strings.Replace(c, "\\n", "\n", -1)
+
+				//index := strings.Index(line, "text:")
+				log.Debugln(c)
+				log.Debugln("-----------------")
+				return c
+
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Debugln("Error reading response body:", err)
+			return "err"
+		}
+
+		return "err"
+
+	} else {
+		fmt.Println("Request failed. Status code:", resp.StatusCode)
+		return "err"
+	}
 }
 
 func HandleGenerateRequest(w http.ResponseWriter, r *http.Request) {
@@ -99,7 +229,7 @@ func HandleGenerateRequest(w http.ResponseWriter, r *http.Request) {
 func fun_post_Request(w http.ResponseWriter, body []byte) {
 
 	// 设置响应头，表明是一个流式响应
-	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Transfer-Encoding", "chunked")
 
 	/**
@@ -137,7 +267,9 @@ func Generatetext(w http.ResponseWriter, body []byte) {
 	var message_txt string
 	for _, msg := range data.Messages {
 		log.Debugf("Role: %s, Content: %s\n", msg.Role, msg.Content)
-		message_txt = fmt.Sprintf("%s\nRole: %s, Content: %s\n", message_txt, msg.Role, msg.Content)
+		//message_txt = fmt.Sprintf("%s\n\n【%s】:%s\n", message_txt, msg.Role, msg.Content)
+		message_txt = fmt.Sprintf("%s\n\n%s\n", message_txt, msg.Content)
+
 	}
 
 	// // 打印请求体
@@ -149,10 +281,9 @@ func Generatetext(w http.ResponseWriter, body []byte) {
 
 func google_ai(w http.ResponseWriter, Prompt string) {
 
+	log.Debugln("对模型的提问", Prompt)
 	// Ensure client is initialized
 	clientInit.Do(InitializeGenerativeClient)
-
-	log.Debugln("对模型的提问", Prompt)
 
 	payload := fmt.Sprintf(`{"contents":[{"parts":[{"text": "%s"}]}]}`, Prompt)
 
@@ -197,6 +328,9 @@ func printResponse(w http.ResponseWriter, resp *http.Response) {
 			// 去除多余符号
 			c = strings.Replace(c, "\\n", "\n", -1)
 
+			// /t 替换为4个空格
+			c = strings.Replace(c, "\t", "    ", -1)
+
 			//index := strings.Index(line, "text:")
 			log.Debugln(c)
 			stream_retrn(w, c)
@@ -214,18 +348,50 @@ func printResponse(w http.ResponseWriter, resp *http.Response) {
 
 // txt 文本，转流式 返回
 func stream_retrn(w http.ResponseWriter, datatmp string) {
+	var jsondata []byte
 	for _, char := range datatmp {
-		fmt.Fprintf(w, "%c", char)
+		//chunk: data:  {"id":"chatcmpl-ATdRBnZVzP4krnreNZ14OYLYqWyM5","object":"chat.completion.chunk","created":1703566659,"model":"gpt-3.5-turbo","choices":[{"index":0,"delta":{"content":"？"},"finish_reason":null}]}
+		//chunk: data:  {"id":"chatcmpl-ATdRBnZVzP4krnreNZ14OYLYqWyM5","object":"chat.completion.chunk","created":1703566659,"model":"gpt-3.5-turbo","choices":[{"index":0,"delta":{},"finish_reason":"stop"}]}
+		//chunk: data:[DONE]
+
+		/**
+		普通
+		结束
+		完成
+		**/
+
+		// 组装结构
+		myData := MyStruct{
+			Choices: []Choice{
+				{
+					Delta: Delta{
+						Content: string(char),
+					},
+				},
+			},
+		}
+		// 将结构体转换为 JSON 字符串
+		jsonString, err := json.Marshal(myData)
+		if err != nil {
+			fmt.Println("转换为 JSON 字符串失败:", err)
+			return
+		}
+
+		jsondata = []byte(fmt.Sprintf("data: %s\n\n", jsonString))
+		w.Write(jsondata)
 		w.(http.Flusher).Flush() // 刷新缓冲区，将数据发送到客户端
 
-		// 模拟处理延迟，实际中根据需要调整
-		//time.Sleep(500 * time.Millisecond)
 	}
+	jsondata = []byte("data:[DONE]\n\n")
+	w.Write(jsondata)
+	w.(http.Flusher).Flush() // 刷新缓冲区，将数据发送到客户端
+
 }
 
 // InitializeGenerativeClient initializes the generative AI client once.
 func InitializeGenerativeClient() {
 	post_url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:streamGenerateContent?key=" + os.Getenv("GEMINI_API_KEY")
+	post_url_ = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + os.Getenv("GEMINI_API_KEY")
 
 	if os.Getenv("ALL_PROXY") != "" {
 		// 设置代理地址
